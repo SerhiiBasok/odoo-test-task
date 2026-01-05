d5672637ab07c8dbddda732d28d0c74195d081e1
Serhii Basok <sergeybsk19@gmail.com>
2026-01-05 13:46:18 +0200

    Initial odoo core

---
+++ custom_modules/my_test_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'My Test Module',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'Test module for Odoo setup',
+    'description': 'A simple module to verify custom modules path',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
