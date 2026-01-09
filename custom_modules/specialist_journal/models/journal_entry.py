5170e446c7f6f2591b84498be8f79686d0757242
Serhii Basok <sergeybsk19@gmail.com>
2026-01-09 11:51:31 +0200

    fix: field indentation

--- custom_modules/specialist_journal/models/journal_entry.py
+++ custom_modules/specialist_journal/models/journal_entry.py
@@ -6,6 +6,7 @@ class SpecialistJournalEntry(models.Model):
     _name = "specialist.journal.entry"
     _description = "Note of specialist journal"
     _order = "date desc"
+
     date = fields.Datetime(string="Date", default=lambda self: fields.Datetime.now())
     user_id = fields.Many2one(
         "res.users", string="Specialist", default=lambda self: self.env.user
